ad468ddf4ae263104f3b42d108281ab920075e6b
Minori-Takeuchi <shato.min0716@gmeail.com>
2023-02-15 16:10:13 +0900

    first commit

---
+++ Takeuchi_backend/share-backend/app/Http/Controllers/LikeController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Like;
+
+class LikeController extends Controller
+{
+    public function index()
+    {
+        $items = Like::all();
+        return response()->json([
+        'data' => $items
+        ], 200);
+    }
+    public function store(Request $request)
+    {
+        $item = Like::create($request->all());
+        return response()->json([
+        'data' => $item
+        ], 201);
+    }
+    
+    public function destroy(Like $like)
+    {
+        $item = Like::where('id', $post->id)->delete();
+        if ($item) {
+            return response()->json([
+            'message' => 'Deleted successfully',
+            ], 200);
+        } else {
+            return response()->json([
+            'message' => 'Not found',
+            ], 404);
+        }
+    }
+}
---
+++ Takeuchi_backend/share-backend/routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LikeController;
+use App\Http\Controllers\CommentController;
+
+Route::post('auth/login', [LoginController::class, 'login']);
+Route::apiResource('/post', PostController::class);
+Route::apiResource('/like', LikeController::class);
+Route::apiResource('/comment', CommentController::class);
+
